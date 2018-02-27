0e8467eb39be0a26b572a8604ceb91cba91690f7
“m-nakatani” <nakatani.motoki@ditgroup.jp>
2018-02-27 14:40:18 +0900

    capistrano設定3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@ set :rails_env, :production
 #set :branch, 'release/1.0'
 set :branch, 'master'
 
-role :web, "0.0.0.0"
-role :app, "0.0.0.0"
+role :web, "13.114.16.229"
+role :app, "13.114.16.229"
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, "config/unicorn/production.rb"
